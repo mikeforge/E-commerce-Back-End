7e8cad4acb3597f972f797229442c88814c72500
Mike Kelly <mike.forge@gmail.com>
2022-03-16 18:27:20 -0400

    finished up routes and seeding from connection

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const { Category, Product, Tag } = require('./models');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
+
+const seedCategories = require('./seeds/category-seeds');
+const seedProducts = require('./seeds/product-seeds');
+const seedTags = require('./seeds/tag-seeds');
+const seedProductTags = require('./seeds/product-tag-seeds');
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +20,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
+sequelize.sync()
+.then(() => {
+  seedCategories();
+  seedProducts();
+  seedTags();
+  seedProductTags();
+})
+.then(() => {
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
+});
 });
